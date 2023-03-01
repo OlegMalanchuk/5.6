9b6f0a7535f5bebe303a131b0324c54c0ac80cbe
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-03-01 19:52:27 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,34 @@
+
+public class Main
+    {
+    public static void main(String[] args)
+    {
+        System.out.println(" ");
+        System.out.println("1)");
+        int num1  = 11;
+        int num2  = 4;
+        int sum1 = num1 / num2;
+        int sum2 = num1 % num2;
+        System.out.println(sum1);
+        System.out.println(sum2);
+
+        System.out.println(" ");
+        System.out.println("2)");
+        int num =  95;
+        int a = num / 10;
+        int b = num % 10;
+        int sum = a + b ;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(sum);
+
+        System.out.println(" ");
+        System.out.println("3)");
+        double n = 2.49;
+        System.out.println(Math.round(n));
+
+
+
+    }
+}
+
