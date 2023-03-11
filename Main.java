2c81019137eb058f741343ce801a9fab12ad762a
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-03-11 17:13:03 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,12 +5,11 @@ public class Main
     {
         System.out.println(" ");
         System.out.println("1)");
-        int num1  = 11;
+        int num1 = 11;
         int num2  = 4;
         int sum1 = num1 / num2;
         int sum2 = num1 % num2;
-        System.out.println(sum1);
-        System.out.println(sum2);
+        System.out.println("sum1 = "+sum1+", sum2 = "+sum2);
 
         System.out.println(" ");
         System.out.println("2)");
@@ -18,14 +17,13 @@ public class Main
         int a = num / 10;
         int b = num % 10;
         int sum = a + b ;
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(sum);
+        System.out.println(a +"\n"+b+"\n"+sum);
 
         System.out.println(" ");
         System.out.println("3)");
         double n = 2.49;
-        System.out.println(Math.round(n));
+
+        System.out.println(Math.round(a));
 
 
 
