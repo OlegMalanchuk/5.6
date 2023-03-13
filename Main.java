c6d3b7a323dd1a6bdc4d24c22d87efe2e931d67f
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-03-13 11:38:07 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,9 +5,9 @@ public class Main
     {
         System.out.println(" ");
         System.out.println("1)");
-        int num1 = 11;
+        int num1 =  11;
         int num2  = 4;
-        int sum1 = num1 / num2;
+        double sum1 = (double) num1 / num2;
         int sum2 = num1 % num2;
         System.out.println("sum1 = "+sum1+", sum2 = "+sum2);
 
@@ -16,18 +16,14 @@ public class Main
         int num =  95;
         int a = num / 10;// через те що інт заокруглює до коми
         int b = num % 10;//остача від ділення
-        int sum = a + b ;
+        int sum = a + b;
         System.out.println(a +"\n"+b+"\n"+sum);
 
         System.out.println(" ");
         System.out.println("3)");
-        double n = 2.49;
-        int xx ;
-        xx = (int) n;//явне привеення
-        System.out.println(xx);//е можна робити через цю функцію Math.round
-
-
-
+        double n = 2.49 ;
+        int res = (int)(n + 0.50);
+        System.out.println(res);
     }
 }
 
